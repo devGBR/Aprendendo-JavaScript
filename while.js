9969db823b120a57fbad99cde4308df8e6fb926d
devGBR <gfcontroladoriageral@gmail.com>
2022-03-29 12:19:12 -0300

    while

---
+++ while.js
@@ -0,0 +1,24 @@
+function contar(){
+    var x = 5
+    var produto = 4
+    var divisao = 14
+
+    var conta = parseFloat(produto*=x++)
+    alert(conta)
+//     var count = 0;
+
+// while(count<=100){
+//     if(count%3 == 0)
+//     document.writeln(count+"<br />");
+//     count+=3
+// //    count = count + 1;
+// }
+}
+
+
+prompt.html
+ function enviar(){
+     var nome = document.getElementById("text").value;
+     var idade = document.getElementById("idade").value;
+    document.write("Welcome, ", nome, "<br>Idade: ", idade ," anos");
+ }
